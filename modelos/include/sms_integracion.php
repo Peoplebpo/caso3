a6cbc19a2f36a059a2e7d45b4ef04976bc8c542a
foag1996 <foalvarezg@ufpso.edu.co>
2021-12-28 15:18:52 -0500

    sms integracion

---
+++ modelos/include/sms_integracion.php
@@ -0,0 +1,35 @@
+<?php
+require('../conexion/conexion.php');
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+$dominio         = (isset($_POST['dominio'])) ? $_POST['dominio'] : '';
+$campana_id      = (isset($_POST['campana_id'])) ? $_POST['campana_id'] : '';
+$access_token    = (isset($_POST['access_token'])) ? $_POST['access_token'] : '';
+
+
+
+
+$opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
+$id = (isset($_POST['id'])) ? $_POST['id'] : '';
+
+
+switch($opcion){
+  
+    case 2:        
+        $consulta = "UPDATE sms_integracion SET dominio='$dominio', campana_id='$campana_id', access_token='$access_token' WHERE id='$id' ";		
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();        
+        
+        $consulta = "SELECT * FROM sms_integracion WHERE id='$id' ";       
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+        break;
+  
+    }
+
+print json_encode($data, JSON_UNESCAPED_UNICODE);//envio el array final el formato json a AJAX
+$conexion=null;
+
+
--- vistas/modulos/sms.php
+++ vistas/modulos/sms.php
@@ -43,6 +43,8 @@
                         include 'vistas/modulos/modales/modal_sms.php';
                       ?>
 
+                      <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalInt" data-bs-whatever="@mdo">Integración</button>
+
 
                     </div>
                   </div>        
@@ -65,3 +67,95 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+  <!-- modal -->
+
+  <div class="modal fade" id="modalInt" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="AgregarProductos" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                <div class="modal-header" style="background:#3c8dbc; color:white">
+                    <h5 class="modal-title" id="AgregarProductos">INTEGRACIÓN VOXIMPLANT</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                <form id="formproductos">
+
+                      <div class="row">
+
+                          <div class="col-lg">
+
+                              <div class="form-group">
+                          
+                                  <div class="input-group">
+                                  
+                                      <span class="input-group-text" id="basic-addon1"><i class="fas fa-edit"></i></span>
+
+                                      <input type="text" class="form-control input-lg" placeholder="Ingresar Dominio" id="dominio" required>
+
+                                  </div> 
+
+                              </div>
+
+                          </div>
+
+                      </div>
+
+                      <div class="row" style="text-align: center;">
+
+                      <div class="col-lg">
+
+                              <div class="form-group">
+                    
+                                  <div class="input-group">
+                                  
+                                      <span class="input-group-text" id="basic-addon1"><i class="fas fa-file-signature"></i></span>
+
+                                      <input type="text" class="form-control input-lg" placeholder="Ingresar Campaña ID" id="campana_id" required>
+
+                                  </div>
+
+                              </div>
+
+                          </div>
+
+
+                      </div>
+
+                      <div class="row">
+
+                          <div class="col-lg">
+
+                              <div class="form-group">
+                    
+                                  <div class="input-group">
+                                  
+                                      <span class="input-group-text" id="basic-addon1"><i class="fas fa-signature"></i></span>
+
+                                      <input type="text" class="form-control input-lg" placeholder="Ingresar Access Token" id="access_token" required>
+
+                                  </div>
+
+                              </div>
+
+                          </div>
+
+                      </div>
+
+                     
+
+                </div>
+
+                  <div class="modal-footer">
+
+                    <button type="submit" id="btnguardar" class="btn btn-success">Guardar</button>
+
+                  </div>
+      
+                </form>
+
+                </div>
+            </div>
+        </div>
+
+  <!-- fin modal -->
