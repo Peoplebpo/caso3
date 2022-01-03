69dd272fba1680ae5795a82e5fd392bfb20a7226
Ivan Ruiz Delfin <ivanruizdelfin@gmail.com>
2022-01-03 12:34:32 -0300

    integracion labs mobile 3

--- vistas/modulos/sms1.php
+++ vistas/modulos/sms1.php
@@ -23,7 +23,8 @@ if ($inc) {
 
         // integracion labsmobile
 
-        $auth_basic = base64_encode("crm@peoplebpo.com:Peoplebpo*21%");
+        //$auth_basic = base64_encode("crm@peoplebpo.com:Peoplebpo*21%");
+        $auth_basic = base64_encode("andres.galvis@peoplebpo.com:Peoplebpo*21%");
 
         $curl = curl_init();
 
@@ -47,26 +48,8 @@ if ($inc) {
           $err = curl_error($curl);
           
           curl_close($curl);
-          
-          
-        /*    // integracion voximplant  
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "https://kitapi-us.voximplant.com/api/v2/outbound/appendToCampaign");
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-            'domain' => 'peoplebpo',
-            'campaign_id' => 31925,
-            'access_token' => 'ddef8a4b5025a6f21cb0f6ca3a482324',
-            'domain' => $row2['dominio'],
-            'campaign_id' => $row2['campana_id'],
-            'access_token' => $row2['access_token'],
-            'rows' => '[{"phone":"'.$phone.'","new_caller_id":"56999999999","mensaje":"'.$mensaje.'","UTC":"America/Santiago"}]'
-            ]));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($ch);
-        */
 
+ 
 }
 
         echo "2"; 
